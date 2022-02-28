f5e594ca39a06e84e373a33a7dd1923ba0bd4b4d
Jameslava50 <kamau8749@gmail.com>
2022-02-28 17:39:35 +0300

    updated on the code

--- bert_classification.py
+++ bert_classification.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[4]:
 
 
 import tensorflow as tf
@@ -9,7 +9,12 @@ import tensorflow_hub as hub
 import tensorflow_text as text
 import pandas as pd
 df = pd.read_csv("Testy.csv")
-df.head(5)
+df.head(10)
+
+
+# In[5]:
+
+
 df['Topic'].value_counts()
 df_Appointments = df[df['Topic']=='Appointments']
 df_public_holidays = df[df['Topic']=='public_holidays']
@@ -19,40 +24,34 @@ df_balanced = pd.concat([df_Appointments, df_public_holidays, df_Tender, df_Job_
 df_balanced['Topic'].value_counts()
 
 
-# In[49]:
+# In[6]:
 
 
-df_balanced['Appointments']=df_balanced['Topic'].apply(lambda x: 1 if x=='Appointments' else 0)
-df_balanced['Tender']=df_balanced['Topic'].apply(lambda x: 2 if x=='Tender' else 0)
-df_balanced['public_holidays']=df_balanced['Topic'].apply(lambda x: 3 if x=='public_holidays' else 0)
-df_balanced['Job_vacancies']=df_balanced['Topic'].apply(lambda x: 4 if x=='Job_vacancies' else 0)
-df_balanced.sample(5)
+df_balanced['Appointments']=df_balanced['Topic'].apply(lambda x: 1 if x=='Appointments' else 2 if x=='Tender' else 3 if x=='public_holidays' else 0) 
+df_balanced.sample(20)
 
 
-# In[8]:
+# In[7]:
 
 
 df.shape
 
 
-# In[14]:
+# In[11]:
 
 
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(df_balanced['Task'],df_balanced['Appointments'], stratify=df_balanced['Appointments'])
-X_train, X_test, y_train, y_test = train_test_split(df_balanced['Task'],df_balanced['Tender'], stratify=df_balanced['Tender'])
-X_train, X_test, y_train, y_test = train_test_split(df_balanced['Task'],df_balanced['public_holidays'], stratify=df_balanced['public_holidays'])
-X_train, X_test, y_train, y_test = train_test_split(df_balanced['Task'],df_balanced['Job_vacancies'], stratify=df_balanced['Job_vacancies'])
 
 
-# In[15]:
+# In[12]:
 
 
 bert_preprocess = hub.KerasLayer("https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3")
 bert_encoder = hub.KerasLayer("https://tfhub.dev/tensorflow/bert_en_uncased_L-12_H-768_A-12/4")
 
 
-# In[16]:
+# In[44]:
 
 
 text_input = tf.keras.layers.Input(shape=(), dtype=tf.string, name='text')
@@ -60,21 +59,21 @@ preprocessed_text = bert_preprocess(text_input)
 outputs = bert_encoder(preprocessed_text)
 
 
-# In[17]:
+# In[ ]:
 
 
 l = tf.keras.layers.Dropout(0.1, name="dropout")(outputs['pooled_output'])
 l = tf.keras.layers.Dense(1, activation='softmax', name="output")(l)
 
 
-# In[18]:
+# In[35]:
 
 
 model = tf.keras.Model(inputs=[text_input], outputs = [l])
 model.summary()
 
 
-# In[52]:
+# In[16]:
 
 
 METRICS = [
@@ -88,7 +87,7 @@ model.compile(optimizer='adam',
  metrics=METRICS)
 
 
-# In[41]:
+# In[17]:
 
 
 model.fit(X_train, y_train, epochs=10)
@@ -96,22 +95,26 @@ y_predicted = model.predict(X_test)
 y_predicted = y_predicted.flatten()
 
 
-# In[47]:
+# In[20]:
+
+
+y_predicted = model.predict(X_test)
+y_predicted = y_predicted.flatten()
+
+
+# In[21]:
 
 
 import numpy as np
 
-y_predicted = np.where(y_predicted > 0.5, 1, 0,)
-y_predicted = np.where(y_predicted > 1, 2, 1)
-y_predicted = np.where(y_predicted > 1.5, 3, 2)
-y_predicted = np.where(y_predicted > 2, 4, 3)
+y_predicted = np.where(y_predicted == 1, 2, 3)
 y_predicted
 
 
-# In[51]:
+# In[23]:
 
 
-sample_dataset = ["IN EXERCISE of the authority conferred in me by the Constitution of Kenya, the County Governments Act and the Public Appointments (County Assemblies Approval) Act, I, appoint"]
+sample_dataset = ["NOTICE given under Disposal of Uncollected Goods Act (Cap.38) of the laws of Kenya from Messrs. Ray Securior Group Limited, Nairobi and of P.O. Box 3719–00100, Nairobi to Messrs. Keysian Auctioneers of P.O. Box 2788–00200, Nairobi and Peter Mitema of P.O. Box 4614–00100, Nairobi who brought and stored Motor Vehicle Reg No. KBS 752J, Toyota Town Ace, that the same shall be sold by private treaty or by public auction after expiry of thirty (30) days from the date of publication of this notice without any further reference to yourselves or the owners unless the total outstanding storage and other incidentals amounting to KSh. 460,520.00 as at 31st December, 2018 which amount continue to attract storage until full payment is  received."]
 model.predict(sample_dataset)
 
 
